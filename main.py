8e1c07df03c0a47fee74ef3f25414f02bb063b84
Juvenn Woo <machese@gmail.com>
2010-05-16 17:08:31 +0800

    No cache

--- main.py
+++ main.py
@@ -18,29 +18,17 @@ from mudel import Entry, Channel, Featured
 
 class MainPage(webapp.RequestHandler):
   def get(self):
-    template_values = self.get_sections()
-    path = self.template_path("main.html")
-    self.response.out.write(template.render(path,template_values))
-
-  def get_sections(self):
-    sec = memcache.get_multi(
-	["entries",
-	 "sponsors"],
-	key_prefix="sec_")
-    if not sec.get("entries"):
-      sec["entries"] = self.render_sec_entries()
-    if not sec.get("sponsors"):
-      sec["sponsors"] = self.render_sec_sponsors()
-    if memcache.set_multi(sec, key_prefix="sec_"):
-      # Failed to cache some keys. Note that add_multi will
-      # return list of keys which FAILED to cache. Ref SDK doc
-      logging.warning("memcache.set_multi() not succeeded.")
-    return sec
-
-  def render_sec_entries(self):
+    sec = {}
     query = Entry.all().order("-published")
     entries = query.fetch(30)
-    path = self.template_path("_entries.html")
+    sec["entries"] = self.render_sec_entries(entries)
+
+    sec["sponsors"] = self.render_sec_sponsors()
+    path = self.template("main.html")
+    self.response.out.write(template.render(path, sec))
+
+  def render_sec_entries(self, entries):
+    path = self.template("_entries.html")
     return template.render(path, {"entries":entries})
 
   def render_sec_sponsors(self):
@@ -51,10 +39,10 @@ class MainPage(webapp.RequestHandler):
     for cl in cls:
       e = cl.entry_set.order("-published").get()
       if e: entries.append(e)
-    path = self.template_path("_sponsors.html")
+    path = self.template("_sponsors.html")
     return template.render(path, {"entries":entries})
 
-  def template_path(self, filename):
+  def template(self, filename):
     return os.path.join(os.path.dirname(__file__), "templates", filename)
 
 
