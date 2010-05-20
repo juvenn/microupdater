2a7e77bcb9b55868234d4499a7ddb038aad842ed
Juvenn Woo <machese@gmail.com>
2010-05-20 16:02:10 +0800

    no more scheduled sync task

--- croned.py
+++ croned.py
@@ -8,21 +8,12 @@
 
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.ext import webapp
-from google.appengine.api import memcache
-from updater import Updater
 from mudel import Entry
 
 class CronedHandler(webapp.RequestHandler):
   def get(self):
     action = self.request.get("action")
-    if action == "sync":
-      u = Updater.get_by_key_name("reader_river")
-      if u.sync(): 
-	# Free memcache
-	if not memcache.delete_multi(["entries", "sponsors"],
-	    key_prefix="sec_"):
-	  logging.warning("memcache.delete_multi not succeeded")
-    elif action == "cleanup":
+    if action == "cleanup":
       Entry.cleanup()
     else: self.error(403) # access denied
 
